821f05c597e7da3aa3f6806211a86caf923c7856
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-06-11 20:22:25 +0500

    adds properties for mysql, jpa, and banner.txt, along with MyUserRepository interface

---
+++ src/main/java/dev/hamidz/springsecurity/entity/MyUserRepository.java
@@ -0,0 +1,10 @@
+package dev.hamidz.springsecurity.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MyUserRepository extends JpaRepository<MyUser, Long> {
+
+    Optional<MyUser> findByUsername(String username);
+}
